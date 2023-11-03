c56a9b128c9a5de802e1fb9ad04ddea1bb9cd862
Hugo Wood <hx4.5@free.fr>
2023-11-04 00:54:36 +0100

    ♻️ use htmx to control form

--- src/routes/html.tsx
+++ src/routes/html.tsx
@@ -8,14 +8,15 @@ export const get: http.RequestListener = (req, res) => {
   res.writeHead(200, { "Content-Type": "text/html" });
   res.end(
     <div>
-      <form method="post">
+      <script src="https://unpkg.com/htmx.org@1.9.6"></script>
+      <form hx-post="/html" hx-target="#stuff" hx-swap="beforeend">
         <input type="text" name="title"></input>
         <input type="text" name="content"></input>
         <button type="submit">Submit</button>
       </form>
       <article>
         <h1>Stuff</h1>
-        <ul>
+        <ul id="stuff">
           {stuff.map((s) => (
             <li>s</li>
           ))}
@@ -31,6 +32,7 @@ export const post: http.RequestListener = async (req, res) => {
   if (title) {
     stuff.push(title);
   }
-  res.writeHead(302, { location: "/html" });
-  res.end();
+  // res.writeHead(302, { location: "/html" });
+  res.writeHead(200);
+  res.end(<li>{title}</li>);
 };

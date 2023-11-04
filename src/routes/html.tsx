c0e3872ea6cd1f0226fb889193ee6d5d89868125
Hugo Wood <hx4.5@free.fr>
2023-11-04 02:03:50 +0100

    ✨ update title too

--- src/routes/html.tsx
+++ src/routes/html.tsx
@@ -2,19 +2,31 @@ import http from "node:http";
 import * as elements from "typed-html";
 import * as streamConsumers from "node:stream/consumers";
 
+const stuff: string[] = [];
+
 export const get: http.RequestListener = (req, res) => {
   res.writeHead(200, { "Content-Type": "text/html" });
   res.end(
     <div>
       <script src="https://unpkg.com/htmx.org@1.9.6"></script>
-      <form hx-post="/html" hx-target="#stuff" hx-swap="beforeend">
+      <form
+        hx-post="/html"
+        hx-target="#stuff"
+        hx-select="#elem"
+        hx-swap="beforeend"
+        hx-select-oob="#title"
+      >
         <input type="text" name="title"></input>
         <input type="text" name="content"></input>
         <button type="submit">Submit</button>
       </form>
       <article>
-        <h1>Stuff</h1>
-        <ul id="stuff"></ul>
+        <h1 id="title">{stuff.length} things</h1>
+        <ul id="stuff">
+          {stuff.map((thing) => (
+            <li>{thing}</li>
+          ))}
+        </ul>
       </article>
     </div>
   );
@@ -22,7 +34,13 @@ export const get: http.RequestListener = (req, res) => {
 
 export const post: http.RequestListener = async (req, res) => {
   const form = new URLSearchParams(await streamConsumers.text(req));
-  const title = form.get("title");
+  const title = form.get("title") || "untitled";
+  stuff.push(title);
   res.writeHead(200);
-  res.end(<li>{title}</li>);
+  res.end(
+    <div>
+      <h1 id="title">{stuff.length} things</h1>
+      <li id="elem">{title}</li>
+    </div>
+  );
 };

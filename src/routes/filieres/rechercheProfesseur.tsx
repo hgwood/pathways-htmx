b910a5fa3f907c9aa0cf1707c90f85729f229e39
Hugo Wood <hx4.5@free.fr>
2024-04-01 21:58:37 +0200

    🚧 add prof modal : remove active, more profs

--- src/routes/filieres/rechercheProfesseur.tsx
+++ src/routes/filieres/rechercheProfesseur.tsx
@@ -1,10 +1,6 @@
-import * as streamConsumers from "node:stream/consumers";
-import { eq } from "drizzle-orm";
 import { Html } from "@kitajs/html";
 import type { RouteHandler } from "../../utils/route";
-import { html, notFound } from "../../utils/httpResponse";
-import { SemestreTab } from "../../components/SemestreTab";
-import classNames from "classnames";
+import { html } from "../../utils/httpResponse";
 
 export const get: RouteHandler = async (req, res, match, url) => {
   const termeRecherche = url?.searchParams.get("termeRechercheProfesseur");
@@ -16,13 +12,10 @@ export const get: RouteHandler = async (req, res, match, url) => {
   return html(
     res,
     <>
-      {searchResults.map(({ name }, index) => (
+      {searchResults.map(({ name }) => (
         <button
           type="button"
-          class={classNames("list-group-item list-group-item-action", {
-            active: index === 0,
-          })}
-          aria-current={index === 0}
+          class="list-group-item list-group-item-action"
           safe
         >
           {name}
@@ -33,19 +26,57 @@ export const get: RouteHandler = async (req, res, match, url) => {
 };
 
 const professors = [
-  {
-    name: "Ulric Williamson",
-  },
-  {
-    name: "Zena Robinson",
-  },
-  {
-    name: "Morgan Flores",
-  },
-  {
-    name: "Amal Dodson",
-  },
-  {
-    name: "Alexandra Nunez",
-  },
-];
+  { firstName: "Pierre", lastName: "Dupont" },
+  { firstName: "Juliette", lastName: "Lefevre" },
+  { firstName: "Arnaud", lastName: "Mercier" },
+  { firstName: "Marie", lastName: "Laurence" },
+  { firstName: "Lucien", lastName: "Bouchard" },
+  { firstName: "Gabrielle", lastName: "Tardif" },
+  { firstName: "Olivier", lastName: "Renault" },
+  { firstName: "Yvette", lastName: "Poirier" },
+  { firstName: "Édouard", lastName: "Dubois" },
+  { firstName: "Camille", lastName: "Durand" },
+  { firstName: "Bernard", lastName: "Blanchard" },
+  { firstName: "Vivienne", lastName: "Moreau" },
+  { firstName: "Frédéric", lastName: "Charpentier" },
+  { firstName: "Yannick", lastName: "Gagnon" },
+  { firstName: "Sylvie", lastName: "Lemoine" },
+  { firstName: "Gérard", lastName: "Beauchamp" },
+  { firstName: "Céline", lastName: "Rousseau" },
+  { firstName: "Louis-Philippe", lastName: "Gauthier" },
+  { firstName: "Florence", lastName: "Guerin" },
+  { firstName: "Maximilien", lastName: "Faure" },
+  { firstName: "Isabelle", lastName: "Lacroix" },
+  { firstName: "Étienne", lastName: "Fontaine" },
+  { firstName: "Margot", lastName: "Leclerc" },
+  { firstName: "Alphonse", lastName: "Bélanger" },
+  { firstName: "Amandine", lastName: "Delorme" },
+  { firstName: "Jean-François", lastName: "Pelletier" },
+  { firstName: "Mathilde", lastName: "Roy" },
+  { firstName: "Théophile", lastName: "Dufresne" },
+  { firstName: "Delphine", lastName: "Perrault" },
+  { firstName: "Romain", lastName: "Thibault" },
+  { firstName: "Elodie", lastName: "Giroux" },
+  { firstName: "Tristan", lastName: "Chevalier" },
+  { firstName: "Valérie", lastName: "Dupuis" },
+  { firstName: "Augustin", lastName: "Lafontaine" },
+  { firstName: "Rosalie", lastName: "Tremblay" },
+  { firstName: "Hiroshi", lastName: "Tanaka" },
+  { firstName: "Sofia", lastName: "Rodriguez" },
+  { firstName: "Liam", lastName: "O'Sullivan" },
+  { firstName: "Rajeev", lastName: "Gupta" },
+  { firstName: "Yulia", lastName: "Ivanova" },
+  { firstName: "Chen", lastName: "Wei" },
+  { firstName: "Abdullah", lastName: "Al-Rashid" },
+  { firstName: "Lucia", lastName: "De La Cruz" },
+  { firstName: "Dimitri", lastName: "Petrov" },
+  { firstName: "Isabella", lastName: "Rossi" },
+  { firstName: "Jackson", lastName: "Smith" },
+  { firstName: "Akiko", lastName: "Yamamoto" },
+  { firstName: "Juan Carlos", lastName: "Mendoza" },
+  { firstName: "Olga", lastName: "Kuznetsova" },
+  { firstName: "Kwame", lastName: "Nkrumah" },
+].map((professeur) => ({
+  ...professeur,
+  name: `${professeur.firstName} ${professeur.lastName.toUpperCase()}`,
+}));

4d1f6848f1936919ea4e67a0be8a17eca199b75a
Hugo Wood <hx4.5@free.fr>
2024-05-11 00:16:23 +0200

    ♻️ maybe improve autocomplete ?

--- src/components/Autocomplete.tsx
+++ src/components/Autocomplete.tsx
@@ -1,12 +1,15 @@
-import { randomBytes } from "crypto";
 import Html, { type Component } from "@kitajs/html";
 import { Spinner } from "./Spinner";
 
 export const Autocomplete: Component<{
+  id: string;
   name: string;
   lienRecherche: string;
-}> = ({ name, lienRecherche, ...props }) => {
-  const autocompleteId = "autcomplete" + randomBytes(16).toString("hex");
+  value?: string;
+  options?: string[];
+}> = ({ id, name, lienRecherche, value = "", options = [], ...props }) => {
+  const autocompleteId = id;
+  const inputId = autocompleteId + "Input";
   const dataListId = autocompleteId + "DataList";
   return (
     <>
@@ -14,6 +17,7 @@ export const Autocomplete: Component<{
         id={autocompleteId}
         {...props}
         hx-disinherit="*"
+        hx-include="find input"
         class="input-group autocomplete"
       >
         <span class="input-group-text">Ajouter un professeur</span>
@@ -22,35 +26,43 @@ export const Autocomplete: Component<{
           <Spinner />
         </span>
         <input
+          id={inputId}
           name={name}
           type="text"
           list={dataListId}
           autocomplete="off"
           class="form-control"
+          value={value}
           hx-get={lienRecherche}
-          hx-trigger="input changed" // no delay here because https://github.com/bigskysoftware/htmx/issues/1189
-          hx-target={`#${dataListId}`}
+          hx-trigger="input changed delay:200ms"
+          hx-target={`#${autocompleteId}`}
           hx-on-input="event.inputType === 'insertReplacementText' && htmx.trigger(this, 'change', event)"
-          hx-on-change="event.target.value.match(/[0-9]+/) && htmx.trigger(this.parentElement, 'autocomplete', { value: event.target.value }) && (this.value = '')"
+          hx-on-change={`${JSON.stringify(
+            options
+          )}.includes(event.target.value) && htmx.trigger(this.parentElement, 'autocomplete', { value: event.target.value }) && (this.value = '')`}
           hx-indicator={`#${autocompleteId}`}
           hx-sync="closest .autocomplete:abort"
         ></input>
       </div>
-      <datalist id={dataListId}></datalist>
+      <datalist id={dataListId}>
+        {options.map((option) => (
+          <option value={option}></option>
+        ))}
+      </datalist>
       <style>
         {`
-          .autocomplete .htmx-indicator {
-            display: none;
-          }
+        .autocomplete .htmx-indicator {
+          display: none;
+        }
 
-          .autocomplete.htmx-request .htmx-indicator {
-            display: inline;
-          }
+        .autocomplete.htmx-request .htmx-indicator {
+          display: inline;
+        }
 
-          .autocomplete.htmx-request .bi-search {
-            display: none;
-          }
-        `}
+        .autocomplete.htmx-request .bi-search {
+          display: none;
+        }
+      `}
       </style>
     </>
   );
